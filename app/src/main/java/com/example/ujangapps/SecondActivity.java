2dbccfbb3a4f9efd89725dc44450d1da322e2889
androbita <pranadana55@gmail.com>
2023-03-17 14:26:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ujangapps/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.ujangapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public Button btnBack;
+    public TextView txt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        String data;
+        txt2 = findViewById(R.id.txt_2);
+
+        if (savedInstanceState == null){
+            Bundle paket = getIntent().getExtras();
+            if (paket == null){
+                data = null;
+            }else{
+                data = paket.getString("bawa_data");
+                txt2.setText(data);
+            }
+        }
+
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
