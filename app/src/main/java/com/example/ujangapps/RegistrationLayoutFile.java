d8882cd02bd45831bd62b39a23589eb9afc25043
androbita <pranadana55@gmail.com>
2023-03-27 11:04:47 +0700

    Event Handlers

---
+++ app/src/main/java/com/example/ujangapps/RegistrationLayoutFile.java
@@ -0,0 +1,27 @@
+package com.example.ujangapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationLayoutFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_layout_file);
+    }
+
+    public void pencet(View view) {
+        String label = ((Button)v).getText().toString();
+        Log.i("Status", label + "Telah ditekan");
+    }
+
+    public void tonjok(View view) {
+        String label = ((Button)v).getText().toString();
+        Log.i("Status", label + "Telah ditekan");
+    }
+}
